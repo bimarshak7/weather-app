ebbd829c1915737e6aa1d3d0f50facca3e3f0677
bimarshak7 <pas076bct010@wrc.edu.np>
2020-07-07 21:04:31 +0545

    Updates

---
+++ precache-manifest.7faf94720032d78f0b7be3947e17044a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaac972d4661e7087ea6857b45b152f",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "27ecf036254a8efc6679",
+    "url": "/weather-app/static/css/main.5c4953a7.chunk.css"
+  },
+  {
+    "revision": "0b056972721afcdcb168",
+    "url": "/weather-app/static/js/2.9a09b48c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.9a09b48c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ecf036254a8efc6679",
+    "url": "/weather-app/static/js/main.fe150363.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  }
+]);
